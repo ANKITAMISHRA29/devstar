008319205654fc173363662de4abf8dc2c4cb569
02skysane <prachikumathekar84@gmail.com>
2024-07-19 21:34:14 +0530

    Math Toolkit
    Created Number System Converter.

--- src/routes/(tools)/math-toolkit/+page.svelte
+++ src/routes/(tools)/math-toolkit/+page.svelte
@@ -1,27 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Number System Converter</title>
+   <!--<link rel="stylesheet" href="style.css">-->
+   <!--<script src="script.js" defer></script>-->
+</head>
+<body>
+
+  <!--</header>-->
+   <main>
+      <h1 style="color:white;">Number System Converter</h1>
+      <div id="select-container">
+         <select id="from-select">
+            <option value="from-binary">Binary</option>
+            <option value="from-decimal">Decimal</option>
+            <option value="from-hexadecimal">Hexadecimal</option>
+            <option value="from-octal">Octal</option>
+         </select>
+         <span style="color:white">to</span>
+         <select id="to-select">
+            <option value="to-binary">Binary</option>
+            <option value="to-decimal">Decimal</option>
+            <option value="to-hexadecimal">Hexadecimal</option>
+            <option value="to-octal">Octal</option>
+         </select>
+      </div>
+      <div id="input-container">
+         <input type="text" id="from-input" placeholder="Binary Number">
+         <span id="error"></span>
+      </div>
+      <button id="convert-button">Convert</button>
+      <input type="text" id="to-input" placeholder="Binary Number" readonly>
+   </main>
+</body>
+
 <script>
-	
 const fromSelected = document.getElementById("from-select");
 const toSelected = document.getElementById("to-select");
 const from = document.getElementById("from-input");
 const to = document.getElementById("to-input");
 const error = document.getElementById("error");
-
 let fromNS = "Binary", toNS = "Binary";
-
 fromSelected.addEventListener("change", function () {
    fromNS = fromSelected.options[fromSelected.selectedIndex].text;
    from.placeholder = fromNS + " Number";
 });
-
 toSelected.addEventListener("change", function () {
    toNS = toSelected.options[toSelected.selectedIndex].text;
    to.placeholder = toNS + " Number";
 });
-
 from.addEventListener("input", function () {
    error.style.display = "none";
 });
-
 let fromValue;
 document.getElementById("convert-button").addEventListener("click", function () {
    switch (fromNS) {
@@ -43,7 +76,6 @@ document.getElementById("convert-button").addEventListener("click", function () 
             to.value = "";
          }
          break;
-
       case "Decimal":
          fromValue = from.value;
          if (/^[0-9]*$/.test(fromValue)) {
@@ -62,7 +94,6 @@ document.getElementById("convert-button").addEventListener("click", function () 
             to.value = "";
          }
          break;
-
       case "Hexadecimal":
          fromValue = from.value;
          if (/^[0-9a-fA-F]*$/.test(fromValue)) {
@@ -81,7 +112,6 @@ document.getElementById("convert-button").addEventListener("click", function () 
             to.value = "";
          }
          break;
-
       case "Octal":
          fromValue = from.value;
          if (/^[0-7]*$/.test(fromValue)) {
@@ -102,73 +132,10 @@ document.getElementById("convert-button").addEventListener("click", function () 
          break;
    }
 });
-
 </script>
-
-<div class="card gap-16 items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 overflow-hidden rounded-lg">
-	<!-- Add tool here -->
-
-	<!DOCTYPE html>
-	<html lang="en">
-	
-	<head>
-	   <meta charset="UTF-8">
-	   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-	   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	   <title>Number System Converter</title>
-	
-	   <link rel="stylesheet" href="style.css">
-	   <script src="script.js" defer></script>
-	</head>
-	
-	<body>
-		  <div class="navbar">
-			 <img src="logo.png.png" class="logo">
-			  <ul>
-				  <li><a href="#">Home</a></li>
-				  <li><a href="#">About</a></li>
-				  <li><a href="#">Contact</a></li>
-			  </ul>
-	   </div>
-	   <main>
-		  <h1 style="color:white;">Number System Converter</h1>
-		  
-		  <div id="select-container">
-			 <select id="from-select">
-				<option value="from-binary">Binary</option>
-				<option value="from-decimal">Decimal</option>
-				<option value="from-hexadecimal">Hexadecimal</option>
-				<option value="from-octal">Octal</option>
-			 </select>
-			 <span>to</span>
-			 <select id="to-select">
-				<option value="to-binary">Binary</option>
-				<option value="to-decimal">Decimal</option>
-				<option value="to-hexadecimal">Hexadecimal</option>
-				<option value="to-octal">Octal</option>
-			 </select>
-		  </div>
-	
-		  <div id="input-container">
-			 <input type="text" id="from-input" placeholder="Binary Number">
-			 <span id="error"></span>
-		  </div>
-	
-		  <button id="convert-button">Convert</button>
-	
-		  <input type="text" id="to-input" placeholder="Binary Number" readonly>
-	   </main>
-	
-	
-	</body>
-	
-	</html>
-	
-</div>
 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Poppins&display=swap");
-
 * {
   font-family: "Poppins", sans-serif;
   margin: 0;
@@ -219,36 +186,29 @@ main {
   justify-content: center;
   align-items: center;
 }
-
 body {
    background-image: linear-gradient(214deg,
-      #ffffff 14%,
-      #f7c9f8 46%,
-      #a0aea9 86%);
-  background-image: url(num.png);
+      #FFFFFF 14%,
+      #F7C9F8 46%,
+      #A0AEA9 86%);
+      background-image: url(num.png);
   min-height: 100vh;
 }
-
 main {
   background-image: linear-gradient(325deg, #101010, #101010, #101010);
-
-
   padding: 120px 100px;
   border-radius: 10px;
 }
-
 #select-container {
   display: flex;
   justify-content: space-evenly;
   align-items: center;
 }
-
 select {
   margin: 10px 20px;
   padding: 7px 15px;
   outline: none;
 }
-
 input {
   background-color: #fff;
   min-width: 350px;
@@ -259,29 +219,25 @@ input {
   outline: none;
   transition: 0.5s;
 }
-
 input:focus {
   box-shadow: 0px 6px 10px 1px rgb(255, 255, 255);
 }
-
 #convert-button {
   padding: 7px 15px;
   border: none;
   border-radius: 5px;
   cursor: pointer;
   transition: 0.3s transform;
+  background-color: #fff;
 }
-
 #convert-button:active {
   transform: scale(0.9);
 }
-
 #input-container {
   display: flex;
   flex-direction: column;
   justify-content: center;
 }
-
 #error {
   display: none;
   color: #f00;
@@ -289,7 +245,6 @@ input:focus {
   margin: 0 0 10px 30px;
   text-align: start;
 }
-
 #written-by-container {
   display: flex;
   flex-direction: column;
@@ -300,8 +255,8 @@ input:focus {
   padding: 1rem;
   margin-top: 5px;
 }
-
 #written-by-container a {
   color: #000;
 }
 </style>
+</html>
